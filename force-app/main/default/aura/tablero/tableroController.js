93e509934bc12408488ad844ff2a517d02964cd5
mdesisoy <lozano.lemar21@zaragoza.salesianos.edu>
2023-03-08 12:28:23 +0100

    sigo con la tabla lo relaciono con trablero

--- force-app/main/default/aura/tablero/tableroController.js
+++ force-app/main/default/aura/tablero/tableroController.js
@@ -6,13 +6,10 @@
     iniciarJuego : function(component, event, helper) {
         component.set("v.score", 0)
         var action = component.get("c.getRandomNumber");
-        var lastNumber = component.get("v.lastNumber");
-        console.log("lastNumber: " +lastNumber)
         //necesito settear los parametros del método antes
         action.setParams({
             "min": 1,
-            "max": 8,
-            "lastNumber": lastNumber
+            "max": 8
         });
         action.setCallback(this, function(response) {
             var state = response.getState();
@@ -21,8 +18,7 @@
                 var numAleatorio = response.getReturnValue();
                 
                 var randomEvent = $A.get("e.c:random");
-                randomEvent.setParams({"random": numAleatorio, "lastNumber": lastNumber});
-                component.set("v.lastNumber", lastNumber)
+                randomEvent.setParams({"random": numAleatorio});
                 randomEvent.fire();
                 console.log(numAleatorio);
             }
@@ -33,6 +29,8 @@
     //funcion seguir si el boton que ha pulsado es el correcto y para sumar el score
     sigo: function(component, event, helper) {
         var action = component.get("c.getRandomNumber");
+        var lastNumber = component.get("v.lastNumber");
+        // console.log("lastNumber: " +lastNumber)
         action.setParams({
             "min": 1,
             "max": 8,
@@ -54,7 +52,8 @@
                     component.set("v.score", 0);
                     console.log("score", component.get("v.score"));
                 }
-                randomEvent.setParams({"random": numAleatorio});
+                randomEvent.setParams({"random": numAleatorio, "lastNumber": lastNumber});
+                component.set("v.lastNumber", lastNumber);
                 randomEvent.fire();
                 console.log(numAleatorio);
             }
@@ -66,5 +65,16 @@
     terminarJuego: function(component, event, helper) {    
         var score = component.get("v.score");
         alert("Tu score es: " + score);
+
+        //disparar evento pasarTabla
+        var action = $A.get("e.c:pasarTabla");
+        // recuperamos los valores de los campos
+        var score  = component.find("score").getElement().value;
+        // asignamos los valores a los parámetros del evento
+        action.setParams({
+            "score": score
+        });
+        //disparamos el evento
+        action.fire();
     }
 })
